e36b478247981b2af0aff064b1ce7d3aeacc5dfb
fengwei0328 <feng.wei8@zte.com.cn>
2024-12-10 11:21:55 +0800

    Fix duplicate image entries in k8s.io namespaces
    The same imageId underk8s.io is showing multiple results: repo:tag, repo:digest, configID.
    We expect to display only repo:tag, consistent with other namespaces and CRI.
    		e.g.
    		nerdctl -n k8s.io images
    		REPOSITORY    TAG       IMAGE ID        CREATED        PLATFORM       SIZE         BLOB SIZE
    		centos        7         be65f488b776    3 hours ago    linux/amd64    211.5 MiB    72.6 MiB
    		centos        <none>    be65f488b776    3 hours ago    linux/amd64    211.5 MiB    72.6 MiB
    		<none>        <none>    be65f488b776    3 hours ago    linux/amd64    211.5 MiB    72.6 MiB
    
    		expect:
    		nerdctl --kube-hide-dupe -n k8s.io images
    		REPOSITORY    TAG       IMAGE ID        CREATED        PLATFORM       SIZE         BLOB SIZE
    		centos        7         be65f488b776    3 hours ago    linux/amd64    211.5 MiB    72.6 MiB
    Of course, even after deduplicating the images displayed, there are still issues with deleting the images.
    It is necessary to distinguish between repo:tag and configId, as well as repoDigest. Considering the situation with tags,
    we need to ensure that all repo:tags under the same imageId are cleaned up before proceeding to clean up the configId and repoDigest.
    
    see: https://github.com/containerd/nerdctl/issues/3702
    
    Signed-off-by: fengwei0328 <feng.wei8@zte.com.cn>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -42,6 +42,7 @@ type Config struct {
 	Experimental     bool            `toml:"experimental"`
 	HostGatewayIP    string          `toml:"host_gateway_ip"`
 	BridgeIP         string          `toml:"bridge_ip, omitempty"`
+	KubeHideDupe     bool            `toml:"kube_hide_dupe"`
 }
 
 // New creates a default Config object statically,
@@ -61,5 +62,6 @@ func New() *Config {
 		HostsDir:         ncdefaults.HostsDirs(),
 		Experimental:     true,
 		HostGatewayIP:    ncdefaults.HostGatewayIP(),
+		KubeHideDupe:     false,
 	}
 }
