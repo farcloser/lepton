3316cc3351c398dc7be180e79229f937f1a547b0
apostasie <spam_blackhole@farcloser.world>
2025-03-31 00:27:32 -0700

    Shell cleanup
    Signed-off-by: apostasie <spam_blackhole@farcloser.world>

--- Dockerfile.d/test-integration-rootless.sh
+++ Dockerfile.d/test-integration-rootless.sh
@@ -14,7 +14,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# shellcheck disable=SC2034
 set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
 if [[ "$(id -u)" = "0" ]]; then
